b4e2c4aa8cae237dc5f69c62df6a2b99f9e00359
邓俊杰 <104496268@qq.com>
2017-07-22 00:31:51 +0800

    移除无用方法

--- cloud.py
+++ cloud.py
@@ -1,9 +1,6 @@
 # coding: utf-8
 
-from leancloud import HttpsRedirectMiddleware
 from leancloud import Engine
-from leancloud import LeanEngineError
-
 from app import app
 
 
@@ -11,19 +8,9 @@ from app import app
 # app = HttpsRedirectMiddleware(app)
 engine = Engine(app)
 
-
 @engine.define
 def hello(**params):
     if 'name' in params:
         return 'Hello, {}!'.format(params['name'])
     else:
         return 'Hello, LeanCloud!'
-
-
-@engine.before_save('Todo')
-def before_todo_save(todo):
-    content = todo.get('content')
-    if not content:
-        raise LeanEngineError('内容不能为空')
-    if len(content) >= 240:
-        todo.set('content', content[:240] + ' ...')
--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from gevent import monkey
-monkey.patch_all()
-
 import os
-
 import leancloud
 
 from app import app
@@ -20,21 +16,3 @@ leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
 leancloud.use_master_key(False)
 
 application = engine
-
-
-if __name__ == '__main__':
-    # 只在本地开发环境执行的代码
-    from gevent.pywsgi import WSGIServer
-    from geventwebsocket.handler import WebSocketHandler
-    from werkzeug.serving import run_with_reloader
-    from werkzeug.debug import DebuggedApplication
-
-    @run_with_reloader
-    def run():
-        global application
-        app.debug = True
-        application = DebuggedApplication(application, evalex=True)
-        server = WSGIServer(('localhost', PORT), application, handler_class=WebSocketHandler)
-        server.serve_forever()
-
-    run()
